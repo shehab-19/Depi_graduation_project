2668d5ca2bee3830ecb5721c63e35ed3f6699709
Shehab Ahmed <shehabeldeenahmed727@gmail.com>
2025-04-18 04:36:59 +0200

    fixed db env vars problem

--- QRCodeApp/Program.cs
+++ QRCodeApp/Program.cs
@@ -5,28 +5,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure database connection directly from environment variables
+// No dependency on appsettings.json connection strings
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    var host = Environment.GetEnvironmentVariable("DB_HOST") ?? "sql-db";
+    var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "QRCodeDB";
+    var user = Environment.GetEnvironmentVariable("DB_USER") ?? "sa";
+    var password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "password@12345";
+    
+    var connectionString = $"Data Source={host};Initial Catalog={dbName};User ID={user};Password={password};TrustServerCertificate=True";
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
