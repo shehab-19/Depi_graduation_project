bbbc4efea66730092bc00b8341e0674301b6b76e
Mohamed-atef345 <ma4243030@gmail.com>
2025-05-10 23:32:48 +0300

    Modified installation.sh

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
   enable_dns_hostnames = "true"
-  enable_dns_support = "true"
+  enable_dns_support   = "true"
 
   tags = {
     Name = var.vpc-name
@@ -12,25 +12,25 @@ resource "aws_vpc" "main" {
 
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public-subnet-cidr_block
-    availability_zone = var.az
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "public-subnet"
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public-subnet-cidr_block
+  availability_zone       = var.az
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
 
 
 resource "aws_subnet" "public2" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public-subnet-cidr_block2
-    availability_zone = var.az
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "public-subnet2"
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public-subnet-cidr_block2
+  availability_zone       = var.az
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet2"
+  }
 }
 
 resource "aws_subnet" "private1" {
@@ -69,11 +69,11 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    
+
     Name = var.public-rtb-name
   }
 
- route {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
@@ -81,50 +81,50 @@ resource "aws_route_table" "public" {
 
 
 resource "aws_route_table_association" "public" {
-    subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 
 resource "aws_security_group" "web_sg" {
-    name        = "controlplane-sg"
-    description = "Security group for web app"
-    vpc_id      = aws_vpc.main.id
-
-  
-    # web access from anywhere
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port   = 6443
-        to_port     = 6443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  name        = "controlplane-sg"
+  description = "Security group for web app"
+  vpc_id      = aws_vpc.main.id
 
 
-    ingress  {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        }
+  # web access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "server-security-group"
-    }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "server-security-group"
+  }
 
 }
 
@@ -132,7 +132,7 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_key_pair" "mykey" {
   key_name   = "mykey"
-  public_key = file("~/.ssh/deployer.pub")  
+  public_key = file("~/.ssh/deployer.pub")
 }
 
 
@@ -140,48 +140,48 @@ resource "aws_key_pair" "mykey" {
 
 
 resource "aws_instance" "controlplane" {
-  ami           = var.ami
-  instance_type = var.t2-instance_type
-  subnet_id     = aws_subnet.public.id
-  key_name      = aws_key_pair.mykey.key_name
+  ami                    = var.ami
+  instance_type          = var.t2-instance_type
+  subnet_id              = aws_subnet.public.id
+  key_name               = aws_key_pair.mykey.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  depends_on = [ aws_db_instance.default ]
+  depends_on             = [aws_db_instance.default]
   tags = {
     Name = "controlplane"
   }
-  
+
 }
 
 resource "null_resource" "fetch_token" {
-    depends_on = [aws_instance.controlplane]
+  depends_on = [aws_instance.controlplane]
 
-    connection {
-        type     = "ssh"
-        user     = "ubuntu"
-        host     = aws_instance.controlplane.public_ip
-        private_key = file("~/.ssh/deployer")
-    }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = aws_instance.controlplane.public_ip
+    private_key = file("~/.ssh/deployer")
+  }
 
-     provisioner "file" {
+  provisioner "file" {
     source      = "../QRCode_APP_Chart"
     destination = "/home/ubuntu/"
   }
 
-    provisioner "remote-exec" {
-      inline = [
-        "echo 'export DB_HOST=${aws_db_instance.default.endpoint}' >> ~/.bashrc",
-        "echo 'export DB_USER=${data.aws_ssm_parameter.db-username.value}' >> ~/.bashrc",
-        "echo 'export DB_NAME=${data.aws_ssm_parameter.db-name.value}' >> ~/.bashrc",
-        "echo 'export DB_PASSWORD=${data.aws_ssm_parameter.db-password.value}' >> ~/.bashrc",
-        "source /home/ubuntu/.bashrc"
-      ]
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'export DB_HOST=${aws_db_instance.default.endpoint}' >> ~/.bashrc",
+      "echo 'export DB_USER=${data.aws_ssm_parameter.db-username.value}' >> ~/.bashrc",
+      "echo 'export DB_NAME=${data.aws_ssm_parameter.db-name.value}' >> ~/.bashrc",
+      "echo 'export DB_PASSWORD=${data.aws_ssm_parameter.db-password.value}' >> ~/.bashrc",
+      "source /home/ubuntu/.bashrc"
+    ]
+  }
 
-    provisioner "remote-exec" {
-      script = "./installation.sh"
-    }
+  provisioner "remote-exec" {
+    script = "./installation.sh"
+  }
 
-   
+
 }
 
 
